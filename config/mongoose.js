4d3ae73124c092c04f91446da83067547685629c
Vishalecs <95082470+Vishalecs@users.noreply.github.com>
2023-08-01 23:08:15 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb+srv://vishalecs:Vishal123321@cluster0.ded7prj.mongodb.net/', {
+  .connect('mongodb+srv://vishalecs:Vishal123321@cluster0.ded7prj.mongodb.net/cell', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
